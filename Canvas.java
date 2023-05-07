b54cc6768f227968d31a3559bcec09590a17d0f3
miguelgddr09 <122293245+miguelgddr09@users.noreply.github.com>
2023-05-06 19:58:19 -0600

    Update Canvas.java

--- Canvas.java
+++ Canvas.java
@@ -1,6 +1,9 @@
+package finalProject;
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
@@ -9,10 +12,11 @@ public class Canvas extends JPanel {
 	
 	private static BufferedImage imagen;
 	private static Graphics gfx;
-	private final int PIXEL_SIZE = 40;
-	private final int ROWS = 50;
-	private final int COLUMNS = 50;
+	private final int PIXEL_SIZE=40;
+	private final int ROWS = 20;
+	private final int COLUMNS = 25;
 	private Color[][] casillas = new Color[ROWS][COLUMNS];
+	
 	
 	private void resetCanvas() {
 		for (int i = 0; i < ROWS; i++) {
@@ -29,14 +33,9 @@ public class Canvas extends JPanel {
 		repaint();
 	}
 	
-	public void setColor(int row, int col, Color color) {
-		casillas[row][col] = color;
-		repaint();
-	}
-	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(imagen, 0, 0, null);
+		g.drawImage(imagen, 0, 0, true);
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLUMNS; j++) {
 				g.setColor(Color.BLACK);
@@ -66,7 +65,30 @@ public class Canvas extends JPanel {
 	
 	 public static void main(String[] args) {
         Canvas canvas = new Canvas();
-        canvas.paintComponent();
+        canvas.paintComponent(gfx);
     }
-}
+	
+	/*public void paint() {
+		//drawBackground(PIXEL_SIZE, Color.white);
+		//drawSquareGrid(PIXEL_SIZE, Color.black);
+	}
+	
+	private  void drawSquareGrid(int size, Color c) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {                
+                g.setColor(c);
+                g.drawRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+            }
+        }
+    }
+	
+	private  void drawBackground(int size, Color c) {
+    		for (int i = 0; i < size; i++) {
+            	for (int j = 0; j < size; j++) {
+                	g.setColor(c);
+                	g.fillRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);                
+            	}
+        	}
+        }*/
+
 }
