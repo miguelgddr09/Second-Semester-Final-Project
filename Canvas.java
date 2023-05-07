c67f0e3c88206fb01699ea50163996136a1b6aec
miguelgddr09 <122293245+miguelgddr09@users.noreply.github.com>
2023-05-07 12:58:36 -0600

    Update Canvas.java

--- Canvas.java
+++ Canvas.java
@@ -3,20 +3,24 @@ package finalProject;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
-public class Canvas extends JPanel {
+public class Canvas extends JPanel implements MouseMotionListener{
 	
 	private static BufferedImage imagen;
 	private static Graphics gfx;
-	private final int PIXEL_SIZE=40;
-	private final int ROWS = 20;
+	private final int PIXEL_SIZE = 40;
+	private final int ROWS = 25;
 	private final int COLUMNS = 25;
 	private Color[][] casillas = new Color[ROWS][COLUMNS];
 	
+	//x y cordinates
+	
+	private int x,y;
 	
 	private void resetCanvas() {
 		for (int i = 0; i < ROWS; i++) {
@@ -33,9 +37,14 @@ public class Canvas extends JPanel {
 		repaint();
 	}
 	
+	public void setColor(int row, int col, Color color) {
+		casillas[row][col] = color;
+		repaint();
+	}
+	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(imagen, 0, 0, true);
+		g.drawImage(imagen, 0, 0, null);
 		for (int i = 0; i < ROWS; i++) {
 			for (int j = 0; j < COLUMNS; j++) {
 				g.setColor(Color.BLACK);
@@ -63,32 +72,20 @@ public class Canvas extends JPanel {
 		return imagen;
 	}
 	
-	 public static void main(String[] args) {
-        Canvas canvas = new Canvas();
-        canvas.paintComponent(gfx);
-    }
-	
-	/*public void paint() {
-		//drawBackground(PIXEL_SIZE, Color.white);
-		//drawSquareGrid(PIXEL_SIZE, Color.black);
-	}
-	
-	private  void drawSquareGrid(int size, Color c) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {                
-                g.setColor(c);
-                g.drawRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
-            }
-        }
-    }
-	
-	private  void drawBackground(int size, Color c) {
-    		for (int i = 0; i < size; i++) {
-            	for (int j = 0; j < size; j++) {
-                	g.setColor(c);
-                	g.fillRect(i * PIXEL_SIZE, j * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);                
-            	}
-        	}
-        }*/
+	 //public static void main(String[] args) {
+        //Canvas canvas = new Canvas();
+        //canvas.paintComponent(gfx);
+    //}
 
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
