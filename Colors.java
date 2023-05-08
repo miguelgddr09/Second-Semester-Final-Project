a21a54e4e97731063faa11b29f9496fed872cf24
miguelgddr09 <122293245+miguelgddr09@users.noreply.github.com>
2023-05-07 22:19:42 -0600

    Add files via upload

---
+++ Colors.java
@@ -0,0 +1,22 @@
+package finalProject;
+
+import java.awt.Color;
+
+public class Colors {
+	Color currentColor;
+	private int redValue = 0, greenValue = 0, blueValue = 0;
+	
+	public Color getCurrColor() {
+		return currentColor;
+	}
+	
+	public void setCurrentColor(int x, int y, int z) {
+		Color burner = new Color(x,y,z);
+		redValue = x;
+		greenValue = y;
+		blueValue = z;
+		currentColor = new Color(redValue, greenValue, blueValue);
+		System.out.println(redValue + " "+ greenValue + " " + blueValue);
+		System.out.println(currentColor);
+	}
+}
